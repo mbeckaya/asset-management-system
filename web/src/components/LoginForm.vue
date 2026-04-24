422fcdc98c7802e0be3e4fd3824417473dac6e2a
mbeckaya <mischa.beckaya@gmail.com>
2026-04-24 10:00:26 +0200

    Fix: replace broken submodule web with regular directory

---
+++ web/src/components/LoginForm.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <p>Login Form</p>
+
+    <h2 class="text-red-500 p-4">Test Tailwind</h2>
+
+    <button class="btn btn-primary mx-4">Test DaisyUI</button>
+</template>
---
+++ web/src/pages/LoginPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import LoginForm from "@/components/LoginForm.vue";
+</script>
+
+<template>
+    <h1>Login Page</h1>
+
+    <LoginForm />
+</template>
